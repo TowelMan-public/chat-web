4d403ad3fdd234a0b9e0020c238b6ebdf7be1b8f
TowelMan-public <OdagiriTomoya@outlook.jp>
2021-05-21 06:22:24 +0900

    SecurityConfigの修正（後でまた修正する可能性あり）

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 //ログインエラー時の遷移先 ※パラメーターに「error」を付与
                 .failureUrl(LOGIN_PAGE)
                 //ログイン成功時の遷移先
-                .defaultSuccessUrl(UrlConfig.ROOT_URL + "/see/space/day",true)
+                .defaultSuccessUrl(UrlConfig.ROOT_URL + "/home",true)
                 //ログイン時のキー：ユーザー名
                 .usernameParameter("userIdName")
                 //ログイン時のパスワード
